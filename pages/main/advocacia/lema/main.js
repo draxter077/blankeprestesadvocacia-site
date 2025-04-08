0045d3533e815a062673ce425a52e13ed7650d45
philippe <philippe.idalgoprestes@gmail.com>
2025-04-08 14:19:46 -0300

    background work

--- pages/main/advocacia/lema/main.js
+++ pages/main/advocacia/lema/main.js
@@ -2,20 +2,34 @@ export default function lema(){
     let style = `
         {
             position:absolute;
-            top:0%;
+            top:30%;
             left:0%;
-            display:flex;
-            justify-content:center;
-            align-items:center;
-            width:60%;
-            height:calc(100% - 80px);
-            margin:40px;
+            padding:40px;
+            width:100%;
             color:var(--colorWhite);
-            font-weight:900;
-            font-size:30px;
+            font-size:40px;
+            font-weight:400;
+        }
+        >span{
+            position:relative;
+            width:fit-content;
+        }
+        >span::after{
+            content:"";
+            position:absolute;
+            bottom:0%;
+            left:0%;
+            width:100%;
+            height:3px;
+            background:var(--colorBlue);
         }`
 
     const lema = cE("div", style)
-    lema.innerHTML = "O sentido da vida é construir construção com direção.<br>A gestão jurídica não é só resolver pendências. É garantir a coerência e funcionar como um curador da sua demanda: Que protege. Que cuida. Que amplia."
+    lema.innerHTML = `O sentido da vida é construir construção com direção.<br>
+                        A gestão jurídica não é só resolver pendências.<br>
+                        É garantir a coerência e funcionar como um curador da sua demanda:<br>
+                        <span>Que protege.</span>
+                        <span>Que cuida.</span>
+                        <span>Que amplia.</span>`
     return(lema)
 }
