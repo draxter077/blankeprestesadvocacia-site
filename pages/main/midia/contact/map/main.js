c8d79472ef152a869cee73ffa7e5da0e0fff7625
philippe <philippe.idalgoprestes@gmail.com>
2025-04-11 16:46:37 -0300

    maps

--- pages/main/midia/contact/map/main.js
+++ pages/main/midia/contact/map/main.js
@@ -8,7 +8,7 @@ export default function map(){
         }`
         
     const map = cE("iframe", style)
-    map.src = "https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d7206.228674324559!2d-49.2524682!3d-25.4344407!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1744217620825!5m2!1spt-BR!2sbr"
+    map.src = "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1801.552670557871!2d-49.2529875!3d-25.4347415!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dce4500475d621%3A0x43cfe5dcaa871088!2sR.%20At%C3%ADlio%20B%C3%B3rio%2C%2080%20-%20Cristo%20Rei%2C%20Curitiba%20-%20PR%2C%2080050-250!5e0!3m2!1spt-BR!2sbr!4v1744400696039!5m2!1spt-BR!2sb"
     map.loading = "lazy"
     map.referrerpolicy = "no-referrer-when-downgrade"
     return(map)
